91e9cfa54b550ac3103ed0b6684b022fd93f18a0
leckliss <erickcaique150@hotmail.com>
2024-03-20 19:48:20 -0300

    adicionando animações, mais sections

---
+++ script.js
@@ -0,0 +1,19 @@
+const GitIcon = document.getElementById('github');
+
+GitIcon.addEventListener('mouseenter', function() {
+    this.src = './assets/img/GitHubHover.svg'; 
+});
+
+GitIcon.addEventListener('mouseleave', function() {
+    this.src = './assets/img/GitHub.svg';
+});
+
+const LinkedinIcon = document.getElementById('linkedin');
+
+LinkedinIcon.addEventListener('mouseenter', function() {
+    this.src = './assets/img/LinkedinHover.svg'; 
+});
+
+LinkedinIcon.addEventListener('mouseleave', function() {
+    this.src = './assets/img/Linkedin.svg';
+});
